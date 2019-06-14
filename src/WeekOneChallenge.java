265184af8624e67e048c397b2407d2ce72668cf6
greendb16 <greendb16@gmail.com>
2019-06-14 10:47:19 -0400

    Final Commit

--- src/WeekOneChallenge.java
+++ src/WeekOneChallenge.java
@@ -1,37 +1,66 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class WeekOneChallenge {
     public static void main(String[] args) {
-        Random randomNumber = new Random();
-        int testNumber;
+        while(true){
+                Random randomNumber = new Random();
+
         Scanner key = new Scanner(System.in);
         System.out.println("Would you like to choose a number?  Or use a random one? (Random/Choose)");
         String input = key.nextLine();
+
         if (input.equalsIgnoreCase("random")) {
             int testNumber = randomNumber.nextInt(250) + 1;
-        } else {
-            System.out.println("Enter a Number:\t");
-            testNumber = key.nextInt();
-        }
+            for (int i = 2; i < testNumber; i++) {
+                int remainder = (testNumber % i);
 
 
-        System.out.println(testNumber);
-        for (int i = 2; i < testNumber; i++) {
-            int remainder = (testNumber % i);
+                if (remainder == 0) {
+                    System.out.println(testNumber + "  Is not a prime number.\n");
+                    break;
+                } else if (i == (testNumber - 1) && i != 2) {
+                    System.out.println(testNumber + " Is a Prime number\n");
+                    break;
 
+                }
 
-            if (remainder == 0) {
-                System.out.println(testNumber + "  Is not a prime number.\n");
-                break;
-            } else if (i == (testNumber - 1) && i != 2) {
-                System.out.println(testNumber + " Is a Prime number\n");
-                break;
 
             }
+        } else {
+            System.out.println("Enter a Number:\t");
+            int testNumber = key.nextInt();
+            key.nextLine();
+            for (int i = 2; i < testNumber; i++) {
+                int remainder = (testNumber % i);
 
 
+                if (remainder == 0) {
+                    System.out.println(testNumber + "  Is not a prime number.\n");
+                    break;
+                } else if (i == (testNumber - 1) && i != 2) {
+                    System.out.println(testNumber + " Is a Prime number\n");
+                    break;
+
+                }
+
+
+            }
         }
+        System.out.println("Would you like to try again? (Yes/No)");
+
+        String inputRedo = key.nextLine();
+        if (inputRedo.equalsIgnoreCase("yes")){
+            continue;
+        }
+        else{
+            System.out.println("Goodbye")
+            ;break;}
+
+
+
     }
-}
+}}
 
